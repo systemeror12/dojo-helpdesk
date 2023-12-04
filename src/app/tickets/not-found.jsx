a37116c2c0e08c7d781704487eac3029d156f766
systemeror12 <vrixenmendoza@gmail.com>
2023-12-04 19:48:37 +0800

    added customized 404 not found jsx

---
+++ src/app/tickets/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+export default function NotFound() {
+    return (
+        <main className="text-center">
+            <h2 className="text-3xl">We Hit the Brick Wall</h2>
+            <p>We could not find the ticket you where looking for.</p>
+            <p>Go back to all <Link href="/tickets">tickets.</Link></p>
+        </main>
+    )
+}
